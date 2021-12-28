e6fb86f691e0a27e0b577fd5634617bdcec2dab9
고광표 <kwangpyo@gogwangpyoui-MacBook-Pro.local>
2021-12-28 15:46:45 +0900

    fix: Fix INVALID_FORMAT errorcode

--- src/main/kotlin/com/mediaservice/application/ProfileService.kt
+++ src/main/kotlin/com/mediaservice/application/ProfileService.kt
@@ -7,12 +7,11 @@ import com.mediaservice.exception.ErrorCode
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
-import javax.validation.Valid
 
 @Service
 class ProfileService(private val profileRepository: ProfileRepository) {
     @Transactional(readOnly = true)
-    fun findById(id: UUID): @Valid ProfileResponseDto {
+    fun findById(id: UUID): ProfileResponseDto {
         return ProfileResponseDto.from(
             this.profileRepository.findById(id)
                 ?: throw BadRequestException(ErrorCode.ROW_DOES_NOT_EXIST, "NO SUCH PROFILE $id")
